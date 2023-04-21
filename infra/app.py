2ea9862436fc71f7c031fd89cb82cba7ce41ee65
tomoki.ikawa <tomoki.ikawa@asia-quest.jp>
2023-04-21 19:10:23 +0900

    Add github actions for infra

--- infra/app.py
+++ infra/app.py
@@ -7,7 +7,7 @@ from test.test_stack import TestStack
 
 
 app = cdk.App()
-WorkshopPipelineStack(app, "TestStack",
+TestStack(app, "TestStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
